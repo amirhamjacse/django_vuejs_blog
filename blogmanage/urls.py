a870a2627874061e21822a0731f03ada34b5ea9c
amirhamjacse <amirhamza824@gmail.com>
2024-11-07 10:29:40 +0600

    added views

--- blogmanage/urls.py
+++ blogmanage/urls.py
@@ -4,4 +4,6 @@ from blogmanage import views
 
 urlpatterns = [
     path('list/', views.BlogList.as_view(), name='list'),
+    path('details/<int:pk>', views.BlogRetriveView.as_view(), name='details'),
+    path('create/', views.BlogCreateView.as_view(), name='create'),
 ]
--- blogmanage/views.py
+++ blogmanage/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import BlogPost
 from rest_framework.views import APIView, Response
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import (
+    ListAPIView, RetrieveAPIView, CreateAPIView
+)
 from rest_framework import status
 from .serializers import BlogPostSerializer
 
@@ -9,3 +11,15 @@ from .serializers import BlogPostSerializer
 class BlogList(ListAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+
+class BlogRetriveView(RetrieveAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = 'id'
+
+class BlogCreateView(CreateAPIView):
+    # queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    
+    
