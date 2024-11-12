d17ea880cb3bc6d34932afde6ec891d296654aac
amirhamjacse <amirhamza824@gmail.com>
2024-11-12 14:59:16 +0600

    changes in details comp and homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,10 +2,10 @@
 import NavigationBar from '@/components/NavigationBar.vue';
 import FooterDesign from '@/components/FooterDesign.vue';
 </script>
+
 <template>
   <NavigationBar></NavigationBar>
   <div class="container my-5">
-    <!-- Blog Title -->
     <div class="text-center mb-4">
       <h1>Welcome to My Blog</h1>
       <p class="lead">Latest Posts and Updates</p>
@@ -13,62 +13,30 @@ import FooterDesign from '@/components/FooterDesign.vue';
 
     <!-- Blog Post List -->
     <div class="row">
-
-
-      <!-- <div v-for="cat in blog_data">
-        <div v-for="a in cat">
-          <div class="card">
-            {{ a.title }}
-          </div>
-        </div>
-        
-      </div> -->
-
-
-  <!-- {{ blog_data.results }} -->
-      <div v-for="(post, categoryIndex) in blog_data.results" :key="categoryIndex" class="col-md-12">
-        <!-- {{ category }} -->
-        
-        <!-- Category Header (optional) -->
-        <!-- <h3>Category {{ categoryIndex + 1 }}</h3> -->
-        
-        <!-- Inner loop: Loop through each post within this category -->
-        <div class="row">
-          <!-- <div v-for="(post, postIndex) in category" :key="post.id" class="col-md-12 mb-4"> -->
-            
-            <div v-if="post" class="card my-1">
-              <div class="card-body">
-                <h5 class="card-title">Title: {{ post.title }}</h5>
-                <!-- <p class="card-text">{{ post.blog_description }}</p> -->
-                <!-- <p class="card-text">{{ getExcerpt(post.blog_description, 6) }}</p> -->
-                <!-- <a href="#" class="btn btn-primary">Read More</a> -->
-                <router-link :to="'/details/' + post.id" class="btn btn-primary ">
-                Read More
-                </router-link>
-              <!-- </div> -->
+      <div v-for="(post, index) in paginatedPosts" :key="index" class="col-md-4 p-4">
+        <div class="row">            
+          <div v-if="post" class="card">
+            <div class="card-body">
+              <h5 class="card-title">Title: {{ post.title }}</h5>
+              <img class="p-2" src="https://i.ibb.co/cyy9gTH/blog-camtasia-youtube-thumbnails-1500x1100.png" width="350px;">
+              <router-link :to="'/details/' + post.id" class="btn btn-primary mt-2">Read More</router-link>
             </div>
           </div>
         </div>
-        
       </div>
     </div>
 
-    <!-- Pagination (if needed) -->
+    <!-- Pagination -->
     <nav aria-label="Page navigation">
       <ul class="pagination justify-content-center">
         <li class="page-item" :class="{ disabled: currentPage === 1 }">
-          <a class="page-link" href="#" @click="changePage(currentPage - 1)">Previous</a>
+          <a class="page-link" href="#" @click.prevent="changePage(currentPage - 1)">Previous</a>
         </li>
-        <li
-          v-for="page in totalPages"
-          :key="page"
-          class="page-item" 
-          :class="{ active: currentPage === page }"
-        >
-          <a class="page-link" href="#" @click="changePage(page)">{{ page }}</a>
+        <li v-for="page in totalPages" :key="page" class="page-item" :class="{ active: currentPage === page }">
+          <a class="page-link" href="#" @click.prevent="changePage(page)">{{ page }}</a>
         </li>
         <li class="page-item" :class="{ disabled: currentPage === totalPages }">
-          <a class="page-link" href="#" @click="changePage(currentPage + 1)">Next</a>
+          <a class="page-link" href="#" @click.prevent="changePage(currentPage + 1)">Next</a>
         </li>
       </ul>
     </nav>
@@ -76,33 +44,40 @@ import FooterDesign from '@/components/FooterDesign.vue';
   <FooterDesign></FooterDesign>
 </template>
 
-  <script>
-  import axios from 'axios';
-  const apiUrl = import.meta.env.VITE_API_URL;
-export default{
-    data(){
-      return {
-        blog_data: "",
+<script>
+import axios from 'axios';
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export default {
+  data() {
+    return {
+      blog_data: { results: [] },
+      currentPage: 1,
+      itemsPerPage: 6,
+      totalPages: 1
+    };
+  },
+  mounted() {
+    this.getBlogdata();
+  },
+  methods: {
+    async getBlogdata() {
+      const response = await axios.get(`${apiUrl}blog/list/`);
+      this.blog_data = response.data;
+      this.totalPages = Math.ceil(this.blog_data.results.length / this.itemsPerPage);
+    },
+    changePage(page) {
+      if (page >= 1 && page <= this.totalPages) {
+        this.currentPage = page;
       }
-    },
-    mounted(){
-      this.getBlogdata();
-    },
-    // beforeMount(){
-    //   this.getExcerpt();
-    // },
-    methods: {
-      async getBlogdata(){
-        const response = await axios.get(`${apiUrl}blog/list/`)
-        // const response = axios.get(`http://127.0.0.1:8000/blog/list/`)
-        this.blog_data = response.data;
-        console.log(response.data);
-      },
-    //   getExcerpt(content, wordLimit) {
-    //     console.log('working')
-    //     const words = content.split(' '); // Split content into an array of words
-    //     return words.slice(0, wordLimit).join(' ') + (words.length > wordLimit ? '...' : ''); // Return truncated content
-    // }
-    },
-}
-  </script>
+    }
+  },
+  computed: {
+    paginatedPosts() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.blog_data.results.slice(start, end);
+    }
+  }
+};
+</script>
